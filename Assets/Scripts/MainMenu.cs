622e29df2bb3693ac0e65704df64d8ed7702c85a
dylanstephenalexander <dylanstephenalexander@gmail.com>
2025-04-16 11:42:08 -0700

    Initialize Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame(){
+        SceneManager.LoadScene("SelectNumPlayers");
+    }
+
+    public void OpenTutorial(){
+
+    }
+
+    public void OpenSettings(){
+
+    }
+
+    public void QuitGame(){
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
